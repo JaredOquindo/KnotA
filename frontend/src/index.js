fa4ad99647dc98237fb36bbf177c5b4244b3bdcd
JaredOquindo <joquindo@gbox.adnu.edu.ph>
2025-07-25 00:22:02 +0800

    Initialize Frontend and Backend

---
+++ frontend/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // ← for React 18+
+import App from './App';
+import './index.css'; // optional
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
