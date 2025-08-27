c698fb8c891a37b0f2fc0f81e9295a457318b28f
JaredOquindo <joquindo@gbox.adnu.edu.ph>
2025-08-28 03:02:13 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -40,14 +40,17 @@ app.get("/", (req, res) => {
   res.json({ message: "Knot Backend API is running..." });
 });
 
+// Start server
+const PORT = process.env.PORT || 5000;
+
 // DB Connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log("Connected to Database"))
+.then(() => {
+  console.log("Connected to Database");
+  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+})
 .catch(err => console.error("Database connection error:", err));
 
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
